e4259e03e253cf9f0c30129c8ee1af8ed53f4036
Nana Abeka Otoo <abekaotoo@gmail.com>
2024-02-08 23:53:18 +0100

    added some test on the dataset

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -115,3 +115,122 @@ class TestMNIST(unittest.TestCase):
 
     def tearDown(self):
         del self.data
+
+
+class TestCIFAR10(unittest.TestCase):
+    def setUp(self):
+
+        self.data = get_data(
+            dataset=Dataset.CIFAR10.value,
+            test_size=0.3,
+        )
+
+    def test_size(self):
+
+        self.assertEqual(
+            self.data.X_test.shape[0],
+            1200,
+        )
+        self.assertEqual(
+            self.data.y_test.shape[0],
+            1200,
+        )
+        self.assertEqual(
+            len(self.data.X_train) + len(self.data.X_test),
+            4000,
+        )
+        self.assertEqual(
+            len(self.data.y_train) + len(self.data.y_test),
+            4000,
+        )
+
+    def test_dimensions(self):
+
+        self.assertEqual(
+            self.data.X_train.shape[1],
+            32,
+        )
+        self.assertEqual(
+            self.data.X_train.shape[2],
+            32,
+        )
+        self.assertEqual(
+            self.data.X_test.shape[1],
+            32,
+        )
+        self.assertEqual(
+            self.data.X_test.shape[2],
+            32,
+        )
+        self.assertEqual(
+            self.data.X_train.shape[3],
+            3,
+        )
+        self.assertEqual(
+            self.data.X_test.shape[3],
+            3,
+        )
+
+    def test_unique_labels(self):
+        self.assertEqual(
+            len(torch.unique(self.data.y_train)),
+            10,
+        )
+        self.assertEqual(
+            len(torch.unique(self.data.y_test)),
+            10,
+        )
+
+    def tearDown(self):
+        del self.data
+
+
+class TestCODRNA(unittest.TestCase):
+    def setUp(self):
+        self.data = get_data(
+            dataset=Dataset.COD_RNA.value,
+            test_size=0.3,
+        )
+
+    def test_size(self):
+
+        self.assertEqual(
+            self.data.X_test.shape[0],
+            1000,
+        )
+        self.assertEqual(
+            self.data.y_test.shape[0],
+            1000,
+        )
+        self.assertEqual(
+            len(self.data.X_train) + len(self.data.X_test),
+            5000,
+        )
+        self.assertEqual(
+            len(self.data.y_train) + len(self.data.y_test),
+            5000,
+        )
+
+    def test_dimensions(self):
+
+        self.assertEqual(
+            self.data.X_train.shape[1],
+            8,
+        )
+        self.assertEqual(
+            self.data.X_test.shape[1],
+            8,
+        )
+
+    def test_unique_labels(self):
+        self.assertEqual(
+            len(torch.unique(self.data.y_train)),
+            2,
+        )
+        self.assertEqual(
+            len(torch.unique(self.data.y_test)),
+            2,
+        )
+
+    def tearDown(self):
+        del self.data
