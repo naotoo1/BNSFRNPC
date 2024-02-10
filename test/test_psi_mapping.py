2d898066a7cca433747bf57d7190c04b39049ee1
Nana Abeka Otoo <82911284+naotoo1@users.noreply.github.com>
2024-02-10 20:09:37 +0100

    Update test_psi_mapping.py

--- test/test_psi_mapping.py
+++ test/test_psi_mapping.py
@@ -70,6 +70,28 @@ class TestPSI(unittest.TestCase):
             self.assertNotEqual(normalization_factor_1.any(), normalization__factor_2)
             self.assertEqual(norm_factor.shape[0], 1)
             self.assertEqual(len(norm_factor.size()), 4)
+    
+    def test_channel_wise_normalization(self):
+        for feature_layer in features:
+            normalization_factor_1 = (
+                torch.sqrt(torch.sum(feature_layer**2, dim=1, keepdim=True))
+                + self.epsilon
+            )
+
+            normalization__factor_2 = np.sqrt(
+                feature_layer.size()[2] * feature_layer.size()[3]
+            )
+
+            norm_factor = normalization_factor_1 * normalization__factor_2
+
+            normalised_channels = feature_layer / norm_factor
+
+            flattened_normalised_channels = normalised_channels.view(
+                feature_layer.size()[0], -1
+            )
+
+            self.assertIsInstance(flattened_normalised_channels, torch.Tensor)
+            self.assertEqual(flattened_normalised_channels.shape[0], 1)
 
     def tearDown(self):
         del self.features, self.epsilon
